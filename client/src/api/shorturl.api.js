3f72454c9b8d33f4946c7275c56be9b4922da9ab
Ramchandra Dalvi <rdalvi1210@gmail.com>
2025-09-06 12:14:34 +0530

    Project completed

--- client/src/api/shorturl.api.js
+++
@@ -1,8 +0,0 @@
-import axiosInstance from "../utils/axios.instance";
-
-export const createUrl = async (url) => {
-  const res = await axiosInstance.post("/api/create", {
-    url,
-  });
-  return res.data.shortUrl;
-};
--- server/src/config/config.js
+++
@@ -1,6 +0,0 @@
-export const cookieOptions = {
-  httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: "lax",
-  maxAge: 1000 * 60 * 5, // 5 minutes
-};
